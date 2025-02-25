ce32274050b9f08ca970211235cc9ec0dd21254c
HarunOktayB <harunoktayb@gmail.com>
2025-02-25 16:12:06 +0300

    added sorting functionality

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from . models import Category, Movie
+from .models import Category, Movie
 
-# kullanıcıya gösterilecek olan veriler burada organize edilir.
-# eğer bir veritabanı işlemi yapılacaksa burada yapılır.
-# bu basit bir proje olduğu için class tabanlı view yapısını kullanmadık.
-# view fonksiyonları işimizi görmüş oldu.
 def home(request):
     data = {
         "kategoriler": Category.objects.all(),
@@ -13,14 +9,21 @@ def home(request):
     return render(request, "movies/index.html", data)
 
 def movies(request):
-    data= {
+    sort_order = request.GET.get('sort', 'decreasing')  # Varsayılan: decreasing
+    if sort_order == 'increasing':
+        filmler = Movie.objects.all().order_by('puan')  # Küçükten büyüğe
+    else:
+        filmler = Movie.objects.all().order_by('-puan')  # Büyükten küçüğe
+
+    data = {
         "kategoriler": Category.objects.all(),
-        "filmler": Movie.objects.all()
+        "filmler": filmler,
+        "current_sort": sort_order
     }
     return render(request, "movies/movies.html", data)
 
 def movie_details(request, id):
-    data= {
+    data = {
         "id": Movie.objects.get(id=id),
         "kategoriler": Category.objects.all(),
         "filmler": Movie.objects.all()
@@ -28,13 +31,18 @@ def movie_details(request, id):
     return render(request, "movies/details.html", data)
 
 def category_movies(request, id):
-    kategori = Category.objects.get(id=id)  # Seçilen kategoriyi al
-    filmler = Movie.objects.filter(category=kategori)  # O kategoriye ait filmleri al
+    kategori = Category.objects.get(id=id)  # Seçilen kategori
+    sort_order = request.GET.get('sort', 'decreasing')  # Varsayılan: decreasing
+    if sort_order == 'increasing':
+        filmler = Movie.objects.filter(category=kategori).order_by('puan')  # Küçükten büyüğe
+    else:
+        filmler = Movie.objects.filter(category=kategori).order_by('-puan')  # Büyükten küçüğe
 
     data = {
         "secilen_kategori": kategori,
         "kategoriler": Category.objects.all(),
-        "filmler": filmler
+        "filmler": filmler,
+        "current_sort": sort_order
     }
     
     return render(request, "movies/movies.html", data)
