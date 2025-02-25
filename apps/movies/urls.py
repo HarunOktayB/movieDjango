8dbd8fefaa4bd4ad71a546e4363ccba50f66bcdc
HarunOktayB <harunoktayb@gmail.com>
2025-02-25 10:04:35 +0300

    debugged

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'), 
     path('movies', views.movies, name='movies'),
-    path('movies/<int:id>', views.movie_details, name='details'),   
+    path('movies/<int:id>', views.movie_details, name='details'),
+    path('movies/category/<int:id>/', views.category_movies, name='category_movies'),  # ✅ Eklendi!
+
     #Burada id parametresi alıyoruz ve bu id parametresi ile movie_details fonksiyonuna yönlendiriyoruz.
     #Bu sayede her bir film için ayrı bir sayfa oluşturmuş oluyoruz.
     #id obje id'si oluyor ve bu id'yi kullanarak o id'ye ait olan filmi çekiyoruz. slug görevi görüyor.
